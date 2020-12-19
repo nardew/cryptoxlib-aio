a694fb32ff639544be6f02564650db6b3e3dfb72
nardew <28791551+nardew@users.noreply.github.com>
2020-12-19 18:59:02 +0100

    Eterbase

--- cryptoxlib/clients/bitpanda/exceptions.py
+++ cryptoxlib/clients/bitpanda/exceptions.py
@@ -4,12 +4,12 @@ from cryptoxlib.exceptions import CryptoXLibException
 
 
 class BitpandaException(CryptoXLibException):
-	pass
+    pass
 
 
 class BitpandaRestException(BitpandaException):
-	def __init__(self, status_code: int, body: Optional[dict]):
-		super().__init__(f"Rest API exception: status [{status_code}], response [{body}]")
+    def __init__(self, status_code: int, body: Optional[dict]):
+        super().__init__(f"Rest API exception: status [{status_code}], response [{body}]")
 
-		self.status_code = status_code
-		self.body = body
+        self.status_code = status_code
+        self.body = body
